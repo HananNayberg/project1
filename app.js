4d9bd12aa86f89fb1991948df2c06d529439f6e7
Hanan Nayberg <hnayberg@corp.webroot.com>
2019-05-31 14:42:33 -0600

    added code for appended elements

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello World"); 
+function addPlayer() {
+    //getting the entered string from html
+    let value = document.getElementById('name').value;
+    //putting the value in a text node
+    let newText = document.createTextNode(value);
+    //Creating an element, appending the text node to the element
+    let newEl = document.createElement('p');
+    newEl.appendChild(newText);
+    //inserting the element into the document
+    document.body.insertBefore(newEl,div1);
+}
